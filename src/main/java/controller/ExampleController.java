273e9ab3c6b1f5fce2007bfc99f6539057eb5beb
spiricnra2052019 <106819752+spiricnra2052019@users.noreply.github.com>
2023-04-02 17:40:15 +0200

    Probni kontroler za User-a

---
+++ src/main/java/controller/ExampleController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class ExampleController {
+
+    @GetMapping
+    public void ispisi()
+    {
+        System.out.println("Hello USER-u, brooooooooo");
+    }
+}
