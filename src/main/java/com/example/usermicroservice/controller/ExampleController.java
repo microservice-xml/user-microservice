6be4e492d5835a42b0e512e1018df5d6759e5a79
Gravarica <93195018+Gravarica@users.noreply.github.com>
2023-04-06 13:31:25 +0200

    Added dockerfile

--- src/main/java/com/example/usermicroservice/controller/ExampleController.java
+++ src/main/java/com/example/usermicroservice/controller/ExampleController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.example.usermicroservice.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExampleController {
 
     @GetMapping
-    public void ispisi()
+    public String ispisi()
     {
-        System.out.println("Hello USER-u, brooooooooo");
+        return "Ok";
     }
 }
