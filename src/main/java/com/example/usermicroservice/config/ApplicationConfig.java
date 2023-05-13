6751329373cc4a6cf9a7c2c5442af70e9503cde0
Gravarica <93195018+Gravarica@users.noreply.github.com>
2023-05-13 11:50:53 +0200

    Deleted spring security dependency

--- src/main/java/com/example/usermicroservice/config/ApplicationConfig.java
+++ src/main/java/com/example/usermicroservice/config/ApplicationConfig.java
@@ -1,17 +1,8 @@
 package com.example.usermicroservice.config;
 
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RequiredArgsConstructor
 public class ApplicationConfig {
 
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
 }
