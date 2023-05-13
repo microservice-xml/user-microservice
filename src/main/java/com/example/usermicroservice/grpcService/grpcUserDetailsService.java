7f7bd8ab2254cd9a1d471611c81e917170579e6d
spiricnra2052019 <106819752+spiricnra2052019@users.noreply.github.com>
2023-05-13 22:46:55 +0200

    Feature/user grpc (#3)
    * ready for Kalina
    
    * nes

--- src/main/java/com/example/usermicroservice/grpcService/grpcUserDetailsService.java
+++ src/main/java/com/example/usermicroservice/grpcService/grpcUserDetailsService.java
@@ -2,12 +2,14 @@ package com.example.usermicroservice.grpcService;
 import com.example.usermicroservice.mapper.UserMapper;
 import com.example.usermicroservice.model.User;
 import com.example.usermicroservice.service.UserService;
-import communication.MessageResponse;
+import communication.*;
 import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
-import communication.userDetailsServiceGrpc;
-import communication.UserDetailsResponse;
-import communication.Role;
+
+import java.util.List;
+
+import static com.example.usermicroservice.mapper.UserMapper.convertUserToUserGrpc;
+import static com.example.usermicroservice.mapper.UserMapper.convertUsersToUsersGrpc;
 
 @GrpcService
 @RequiredArgsConstructor
@@ -38,5 +40,24 @@ public class grpcUserDetailsService extends userDetailsServiceGrpc.userDetailsSe
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void changeUserInfo(communication.User request,
+                               io.grpc.stub.StreamObserver<communication.User> responseObserver) {
+                User user = UserMapper.convertUserGrpcToUser(request);
+                User finalUser = userService.changeUserInfo(user);
+
+                responseObserver.onNext(convertUserToUserGrpc(finalUser));
+                responseObserver.onCompleted();
+    }
+
+    @Override
+    public void findAll(communication.EmptyRequest request,
+                        io.grpc.stub.StreamObserver<communication.UserList> responseObserver) {
+                List<User> finalUsers = userService.findAll();
+                UserList userList = UserList.newBuilder().addAllUsers(convertUsersToUsersGrpc(finalUsers)).build();
+                responseObserver.onNext(userList);
+                responseObserver.onCompleted();
     }
 }
--- src/main/java/com/example/usermicroservice/model/User.java
+++ src/main/java/com/example/usermicroservice/model/User.java
@@ -23,7 +23,7 @@ public class User {
     private String location;
     @Column(name = "email", nullable = false)
     private String email;
-    @Column(name = "username" , unique = true, nullable = false)
+    @Column(name = "username" , nullable = false)
     private String username;
     @Column(name = "password", nullable = false)
     private String password;
@@ -35,9 +35,7 @@ public class User {
     private String phoneNumber;
     @Column(name = "penalties")
     private int penalties;
-    @Column(columnDefinition = "ENUM('GUEST','HOST')", name = "role", nullable = false)
+    @Column(columnDefinition = "ENUM('GUEST','HOST')", name = "role")
     @Enumerated(EnumType.STRING)
     private Role role;
-    //Ovde treba promeniti da je role tipa Role ali me zeza MySql vec satima i ne radi ne znam zasto
-
 }
