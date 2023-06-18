2dc98ea3faabaf58a36513912e51759894f43e24
vukomanv <vladimir.vukoman.v@gmail.com>
2023-06-18 14:21:56 +0200

    Added tracing

--- src/main/java/com/example/usermicroservice/grpcService/grpcUserDetailsService.java
+++ src/main/java/com/example/usermicroservice/grpcService/grpcUserDetailsService.java
@@ -8,6 +8,8 @@ import communication.UserList;
 import communication.userDetailsServiceGrpc;
 import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -18,10 +20,12 @@ import static com.example.usermicroservice.mapper.UserMapper.*;
 public class grpcUserDetailsService extends userDetailsServiceGrpc.userDetailsServiceImplBase {
 
     private final UserService userService;
+    private Logger logger = LoggerFactory.getLogger(grpcUserDetailsService.class);
 
     @Override
     public void getUserDetails(communication.UserDetailsRequest request,
                                io.grpc.stub.StreamObserver<communication.UserDetailsResponse> responseObserver){
+        logger.trace("Request to find the user with username {} was made", request.getUsername());
         User user = userService.loadUserByUsername(request.getUsername());
         UserDetailsResponse response = UserMapper.convertUserToUserDetailsResponse(user);
 
@@ -31,7 +35,7 @@ public class grpcUserDetailsService extends userDetailsServiceGrpc.userDetailsSe
     @Override
     public void register(communication.RegisterUser request,
                          io.grpc.stub.StreamObserver<communication.MessageResponse> responseObserver) {
-
+        logger.trace("Request to register with username {} was made", request.getUsername());
         User user = UserMapper.covertRegisterRequestToEntity(request);
         User u = userService.registerUser(user);
         MessageResponse response;
@@ -47,6 +51,7 @@ public class grpcUserDetailsService extends userDetailsServiceGrpc.userDetailsSe
     @Override
     public void changeUserInfo(communication.User request,
                                io.grpc.stub.StreamObserver<communication.User> responseObserver) {
+                logger.trace("Request to change user info for user with id {} was made", request.getId());
                 User user = UserMapper.convertUserGrpcToUser(request);
                 User finalUser = userService.changeUserInfo(user);
 
@@ -57,6 +62,7 @@ public class grpcUserDetailsService extends userDetailsServiceGrpc.userDetailsSe
     @Override
     public void findAll(communication.EmptyRequest request,
                         io.grpc.stub.StreamObserver<UserList> responseObserver) {
+                logger.trace("Request to find all users was made");
                 List<User> finalUsers = userService.findAll();
                 UserList userList = UserList.newBuilder().addAllUsers(convertUsersToUsersExtendedGrpc(finalUsers)).build();
                 responseObserver.onNext(userList);
@@ -66,6 +72,7 @@ public class grpcUserDetailsService extends userDetailsServiceGrpc.userDetailsSe
     @Override
     public void getById(communication.UserIdRequest request,
                         io.grpc.stub.StreamObserver<communication.RegisterUserAvgGrade> responseObserver) {
+        logger.trace("Request to find the user with id {} was made", request.getId());
         User user = userService.getById(request.getId());
         responseObserver.onNext(UserMapper.convertFromUserToRegisterUserAvgGrade(user));
         responseObserver.onCompleted();
@@ -74,6 +81,7 @@ public class grpcUserDetailsService extends userDetailsServiceGrpc.userDetailsSe
     @Override
     public void delete(communication.UserIdRequest request,
                        io.grpc.stub.StreamObserver<communication.MessageResponse> responseObserver) {
+                    logger.trace("Request to delete the user with id {} was made", request.getId());
                     boolean success = userService.deleteUser(request.getId());
                     MessageResponse message;
                     if(success) {
